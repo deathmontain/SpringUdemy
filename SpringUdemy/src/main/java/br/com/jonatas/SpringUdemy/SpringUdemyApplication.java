edbd49ffe72068ceaf1c3ce22c689db9ecffcb62
Jônatas Santos <jonatas.santos@aedu.com>
2020-02-04 11:39:59 -0300

    Primeiro commit

---
+++ SpringUdemy/src/main/java/br/com/jonatas/SpringUdemy/SpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonatas.SpringUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUdemyApplication.class, args);
+	}
+
+}
---
+++ SpringUdemy/src/test/java/br/com/jonatas/SpringUdemy/SpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonatas.SpringUdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
